c6336d969e1a0b07b25b83952766438173863943
Dave Syer <dsyer@vmware.com>
2022-04-08 09:10:07 +0100

    Upgrade to Boot 2.6.6

--- src/main/java/com/example/myproject/GreetingProperties.java
+++ src/main/java/com/example/myproject/GreetingProperties.java
@@ -1,7 +1,6 @@
 package com.example.myproject;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties("greeting")
 public record GreetingProperties(String salutation) {
